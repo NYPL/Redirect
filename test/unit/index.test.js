278e4e4222876b87fcf7dbffb4ad4b3db941ab3e
danamansana <danielappel@nypl.org>
2023-09-22 14:30:49 -0400

    Add replacing of discovery with nypl

--- test/unit/index.test.js
+++ test/unit/index.test.js
@@ -473,7 +473,7 @@ describe('handler', () => {
         multiValueHeaders: { Location: [ "https://nypl.na2.iiivega.com/search/card?recordId=22297361&originalUrl=https%3A%2F%2Fcatalog.nypl.org%2Frecord%3Db22297361%3Fcollection%3Dcirc" ] }
       })
     })
-  
+
     it('should redirect to vega when "circ" is included in multiple collection query params', async function () {
       const event = {
         path: '/record=b22297361',
@@ -553,6 +553,24 @@ describe('handler', () => {
         expect(resp).to.deep.include({
           statusCode: 302,
           multiValueHeaders: { Location: [ 'https://auth.na2.iiivega.com/auth/realms/nypl/protocol/openid-connect/logout?redirect_uri=https%3A%2F%2Fredir-browse.nypl.org%2Fvega-logout-handler%3Fredirect_uri%3D' + encodeURIComponent(encodeURIComponent(validUrl)) ] }
+        })
+      })
+    })
+
+    // Test replacing discovery.nypl.org with nypl.org in redirect
+
+    ;[
+      'https://www.discovery.nypl.org/',
+      'https://discovery.nypl.org/'
+    ].forEach((validUrl) => {
+      it(`should respect redirect_uri=${validUrl}`, async function () {
+        const eventWithRedirect = Object.assign( {}, baseEvent,
+          { multiValueQueryStringParameters: { redirect_uri: [ validUrl ] } }
+        )
+        const resp = await handler(eventWithRedirect, context, (_, resp) => resp);
+        expect(resp).to.deep.include({
+          statusCode: 302,
+          multiValueHeaders: { Location: [ 'https://auth.na2.iiivega.com/auth/realms/nypl/protocol/openid-connect/logout?redirect_uri=https%3A%2F%2Fredir-browse.nypl.org%2Fvega-logout-handler%3Fredirect_uri%3D' + encodeURIComponent(encodeURIComponent('https://www.nypl.org/')) ] }
         })
       })
     })
